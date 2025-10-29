23e9e46f8eea0ba4f15dc08728a5fce2c25afb31
Saransh Gupta <160781953+saransh-g1@users.noreply.github.com>
2025-10-29 23:57:53 +0530

    Refactor Hardhat config for improved structure

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,23 +1,47 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-ethers");
+
+require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+const { PRIVATE_KEY } = process.env;
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  defaultNetwork: "hardhat",
+
   networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+    },
     coreTestnet: {
       url: "https://rpc.test2.btcs.network",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
       chainId: 1114,
     },
   },
+
   paths: {
-    artifacts: "./artifacts",
-    cache: "./cache",
     sources: "./contracts",
     tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    showTimeSpent: true,
   },
 };
